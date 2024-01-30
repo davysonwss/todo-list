d3a9cb4eb34255a371c268ecbb88318b9a54b1bf
Davyson <davysonwss@hotmail.com>
2024-01-30 16:33:38 -0300

    :sparkles: feat: created the array for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { Tasks } from "./components/Tasks";
 
+import { TTask } from "./types/task";
+
 export function App() {
+  const [tasks, setTasks] = useState<TTask[]>([])
+
   return (
     <>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </>
   )
 }
