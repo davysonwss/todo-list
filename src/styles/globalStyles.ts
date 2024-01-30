606d00f2a9c6dfa948b4ffa0ddcad2c2c80c277a
Davyson <davysonwss@hotmail.com>
2024-01-30 15:06:31 -0300

    :lipstick: feat: create global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+#root {
+    --gray-100: #F2F2F2;
+    --gray-200: #D9D9D9;
+    --gray-300: #808080;
+    --gray-400: #333333;
+    --gray-500: #262626;
+    --gray-600: #1A1A1A;
+    --gray-700: #0D0D0D;
+    
+    --blue: #4EA8DE;
+    --blue-dark: #1E6F9F;
+    
+    --purple: #8284FA;
+    --purple-dark: #5E60CE;
+
+    --red-danger: #E25858;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #1A1A1A;
+    color: #808080;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
