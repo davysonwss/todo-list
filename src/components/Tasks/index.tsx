c1f0af831eb86797e1b2477b3853b2a75d2fbfad
Davyson <davysonwss@hotmail.com>
2024-01-30 16:05:52 -0300

    :sparkles: feat: usage component task

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,3 +1,4 @@
+import { Task } from "../Task";
 import { Container, Header } from "./styles";
 
 export function Tasks() {
@@ -14,6 +15,7 @@ export function Tasks() {
           <span>2 de 10</span>
         </div>
       </Header>
+      <Task />
     </Container>
   )
 }
