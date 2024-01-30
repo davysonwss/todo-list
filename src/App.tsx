c6161305679977cad1630155c78f6e823e540660
Davyson <davysonwss@hotmail.com>
2024-01-30 15:55:52 -0300

    :sparkles: feat: using the tasks components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
 
 export function App() {
   return (
     <>
       <Header />
+      <Tasks />
     </>
   )
 }
